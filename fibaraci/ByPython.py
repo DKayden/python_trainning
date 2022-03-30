80f1468f83f57df66144ea817c9c9dbf5097cf67
DKayden <dinhmanhhung1504@gmail.com>
2022-03-30 15:45:44 +0700

    Update ByPython.py

--- fibaraci/ByPython.py
+++ fibaraci/ByPython.py
@@ -1,29 +1,29 @@
 # Không dùng dệ quy
-def fibonancci(n):
-    a = 0
-    b = 1
-    c = 1
-
-    if n < 0:
-        return -1
-    elif n == 0 or n == 1:
-        return n
-    else:
-        for i in range (2,n):
-            a = b
-            b = c
-            c = a + b
-        print(c)
-
-
-#Dùng đệ quy
 # def fibonancci(n):
+#     a = 0
+#     b = 1
+#     c = 1
+
 #     if n < 0:
 #         return -1
 #     elif n == 0 or n == 1:
 #         return n
 #     else:
-#         return fibonancci(n -1) + fibonancci(n -2)
+#         for i in range (2,n):
+#             a = b
+#             b = c
+#             c = a + b
+#         return c
+
+
+#Dùng đệ quy
+def fibonancci(n):
+    if n < 0:
+        return -1
+    elif n == 0 or n == 1:
+        return n
+    else:
+        return fibonancci(n -1) + fibonancci(n -2)
 
 
 while True:
